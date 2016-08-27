946dda5323d219e19c4c340f3cc1a0518846e39a
Eric Brown <quadtree@gmail.com>
2016-08-26 22:41:46 -0700

    Started

---
+++ Source/LD36/LD36GameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LD36.h"
+#include "LD36GameMode.h"
+
+
+
+
---
+++ Source/LD36/LD36GameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "LD36GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LD36_API ALD36GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
