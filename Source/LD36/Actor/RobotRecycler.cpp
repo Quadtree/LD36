ca8b4c4749767a818ceea8a4779c461ca330cb2e
Eric Brown <quadtree@gmail.com>
2016-08-28 02:06:53 -0700

    Conveyor does something

--- Source/LD36/Actor/RobotRecycler.cpp
+++ Source/LD36/Actor/RobotRecycler.cpp
@@ -24,5 +24,31 @@ void ARobotRecycler::Tick( float DeltaTime )
 {
 	Super::Tick( DeltaTime );
 
+	for (auto a : GetComponentsByTag(UPrimitiveComponent::StaticClass(), "Conveyor"))
+	{
+		if (auto p = Cast<UPrimitiveComponent>(a))
+		{
+			TArray<FOverlapResult> res;
+
+			if (GetWorld()->OverlapMultiByObjectType(res, p->GetComponentLocation() + FVector(0,0,50), FQuat::Identity, FCollisionObjectQueryParams::AllDynamicObjects, FCollisionShape::MakeBox(p->GetComponentScale() * FVector(50, 50, 50))))
+			{
+				for (auto o : res)
+				{
+					if (o.Actor.Get() == this) continue;
+					//UE_LOG(LogTemp, Display, TEXT("Conveying=%s"), (o.Actor.IsValid() ? *o.Actor->GetName() : TEXT("???")));
+
+					//if (o.Component.IsValid())
+					//{
+						//o.Component->AddImpulse(FVector(-50, 0, 0), NAME_None, true);
+
+					//}
+					if (o.Actor.IsValid())
+					{
+						o.Actor->SetActorLocation(o.Actor->GetActorLocation() + FVector(25, 0, 0) * DeltaTime);
+					}
+				}
+			}
+		}
+	}
 }
 
