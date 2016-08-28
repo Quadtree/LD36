c70dcfbe67471390fb0099feca8485b1d18de77f
Eric Brown <quadtree@gmail.com>
2016-08-28 01:35:26 -0700

    Robot recycler added

---
+++ Source/LD36/Actor/RobotRecycler.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "RobotRecycler.generated.h"
+
+UCLASS()
+class LD36_API ARobotRecycler : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ARobotRecycler();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	
+	
+};
