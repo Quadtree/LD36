c2ef824c551782db1dac543a142040dc9b2afc18
Eric Brown <quadtree@gmail.com>
2016-08-27 18:09:21 -0700

    Enemy AI controller added

---
+++ Source/LD36/Actor/EnemyAIController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "AIController.h"
+#include "EnemyAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LD36_API AEnemyAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
