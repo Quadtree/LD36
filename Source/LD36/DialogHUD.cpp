d8bd1441468ba32cc6ddd45c7a1e11461d05ebfd
Eric Brown <quadtree@gmail.com>
2016-08-28 11:44:51 -0700

    Collision prevention added

--- Source/LD36/DialogHUD.cpp
+++ Source/LD36/DialogHUD.cpp
@@ -8,11 +8,23 @@ void ADialogHUD::DrawHUD()
 {
 	Super::DrawHUD();
 
+	TArray<FVector> centers;
+
 	for (TActorIterator<ADialog> i(GetWorld()); i; ++i)
 	{
 		if (!i->GetText().IsEmpty())
 		{
 			FVector pos = Project(i->GetActorLocation() + FVector(0,0,150));
+
+			for (auto center : centers)
+			{
+				if (FVector::DistSquaredXY(center, pos) < FMath::Square(200))
+				{
+					pos.Y += 50;
+				}
+			}
+
+			centers.Add(pos);
 
 			if (pos.Z > 0 && pos.X >= -200 && pos.Y >= -200 && pos.X <= Canvas->ClipX + 200 && pos.Y <= Canvas->ClipY + 200)
 			{
