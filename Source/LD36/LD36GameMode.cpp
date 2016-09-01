0c6b744fcfa638448bab5e3f689d8496a3bf95be
Eric Brown <quadtree@gmail.com>
2016-08-31 19:16:02 -0700

    Minor optimization adjustments

--- Source/LD36/LD36GameMode.cpp
+++ Source/LD36/LD36GameMode.cpp
@@ -8,14 +8,13 @@ void ALD36GameMode::BeginPlay()
 	Super::BeginPlay();
 
 #if PLATFORM_HTML5
-	GSystemResolution.RequestResolutionChange(800, 600, EWindowMode::Fullscreen);
-	GEngine->GameUserSettings->ScalabilityQuality.AntiAliasingQuality = 0;
+	GEngine->GameUserSettings->ScalabilityQuality.AntiAliasingQuality = 3;
 	GEngine->GameUserSettings->ScalabilityQuality.EffectsQuality = 3;
 	GEngine->GameUserSettings->ScalabilityQuality.PostProcessQuality = 0;
 	GEngine->GameUserSettings->ScalabilityQuality.ResolutionQuality = 0;
 	GEngine->GameUserSettings->ScalabilityQuality.TextureQuality = 0;
 	GEngine->GameUserSettings->ScalabilityQuality.ViewDistanceQuality = 0;
-	GEngine->GameUserSettings->ScalabilityQuality.ShadowQuality = 0;
+	GEngine->GameUserSettings->ScalabilityQuality.ShadowQuality = 3;
 	GEngine->GameUserSettings->ApplySettings(false);
 #endif
 }
